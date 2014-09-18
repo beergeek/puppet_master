ece7b070c5421b5181af3590266fe164f98a912b
beergeek <brett.gray@puppetlabs.com>
2014-09-18 18:59:23 +1000

    minor additions

--- manifests/mom.pp
+++ manifests/mom.pp
@@ -1,8 +1,18 @@
 class puppet_master::mom (
+  $r10k_enabled  = true,
+  $puppet_remote = undef,
+  $puppet_base   = "${::settings::confdir}/environments",
+  $hiera_remote  = undef,
+  $hiera_base    = "${::settings::confdir}/hieradata",
 ) {
 
   class { 'puppet_master':
-    ca_enabled => true,
+    ca_enabled    => true,
+    r10k_enabled  => $r10k_enabled,
+    puppet_remote => $puppet_remote,
+    puppet_base   => $puppet_base,
+    hiera_remote  => $hiera_remote,
+    hiera_base    => $hiera_base,
   }
 
   # doing this as auth_conf module is a pain in the arse
